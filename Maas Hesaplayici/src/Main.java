bc6d21e438a369128644473e76f4a9f141ca57d9
Oğuzhan Furkan Özdemir <ofozdemir14@gmail.com>
2023-12-24 02:11:10 +0300

    Maaş Hesaplayıcı

---
+++ Maas Hesaplayici/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee isci = new Employee("Oğuzhan",3500,40 ,1997);
+
+        System.out.println(isci.toString());
+    }
+}
